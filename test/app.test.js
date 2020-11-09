f0702b58b739a0f8c4ff78146f8f0a56e3ac625f
Josue Hernandez <moradel82@gmail.com>
2020-11-08 23:32:11 -0700

    More tests to end point

--- test/app.test.js
+++ test/app.test.js
@@ -13,8 +13,27 @@ describe("GET /apps", () => {
         expect(res.body).to.have.lengthOf.at.least(1);
         const book = res.body[0];
         expect(book).to.include.all.keys(
-          'bestsellers_date', 'author', 'description', 'title'
+          "App",
+          "Category",
+          "Rating",
+          "Reviews",
+          "Size",
+          "Installs",
+          "Type",
+          "Price",
+          "Content Rating",
+          "Genres",
+          "Last Updated",
+          "Current Ver",
+          "Android Ver"
         );
       });
+  });
+
+  it("should be 400 if sort is incorrect", () => {
+    return supertest(app)
+      .get("/apps")
+      .query({ sort: "MISTAKE" })
+      .expect(400, "Sort must be one of title or rank");
   });
 });
